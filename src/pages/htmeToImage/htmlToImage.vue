4b46c438572bdab8a85683e9238c5d52d065e2df
彭勇 <pengyong@pengyongs-MacBook-Pro.local>
2017-12-26 09:07:52 +0800

    modify

---
+++ src/pages/htmeToImage/htmlToImage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="htmlToImage">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: '',
+        extends: {},
+        props: {},
+        data() {
+            return {}
+        },
+        computed: {},
+        components: {},
+        watch: {},
+        methods: {},
+        created() {
+        },
+        mounted() {
+        },
+        activated() {
+        }
+    }
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
